f32f9746e5f45f73f8b753d3d249056d9b771df2
nexus <38168516+nexus4880@users.noreply.github.com>
2023-03-11 00:52:29 -0700

    added search bar

--- DayZLootEdit/MainWindow.xaml.cs
+++ DayZLootEdit/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,113 +15,109 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DayZLootEdit
-{
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        private LootTable LootTable;
+namespace DayZLootEdit {
+	/// <summary>
+	/// Interaktionslogik für MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		private LootTable LootTable;
 
+		public MainWindow() {
+			InitializeComponent();
+		}
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+		private void LoadBtn_Click(object sender, RoutedEventArgs e) {
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*";
+			if (dlg.ShowDialog().Value) {
+				LootTable = new LootTable(dlg.FileName);
+				LootTable.LoadFile();
 
-        private void LoadBtn_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog dlg = new OpenFileDialog();
-            dlg.Filter = "XML Files (*.xml)|*.xml|All Files (*.*)|*.*";
-            if (dlg.ShowDialog().Value)
-            {
-                LootTable = new LootTable(dlg.FileName);
-                LootTable.LoadFile();
+				LootList.ItemsSource = LootTable.Loot;
 
-                LootList.ItemsSource = LootTable.Loot;
+				LootList.IsEnabled = true;
+				SaveBtn.IsEnabled = true;
+			}
+		}
 
-                LootList.IsEnabled = true;
-                SaveBtn.IsEnabled = true;
-            }
-        }
+		private void LootList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			LootPercBox.IsEnabled = LootList.SelectedItems.Count > 0;
+		}
 
-        private void LootList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            LootPercBox.IsEnabled = LootList.SelectedItems.Count > 0;
-        }
+		private void PercBtn_Click(object sender, RoutedEventArgs e) {
+			int percentage = 0;
+			bool ok = int.TryParse(PercBox.Text.Replace("%", ""), out percentage);
+			if (!ok) return;
 
-        private void PercBtn_Click(object sender, RoutedEventArgs e)
-        {
-            int percentage = 0;
-            bool ok = int.TryParse(PercBox.Text.Replace("%", ""), out percentage);
-            if (!ok) return;
+			foreach (LootType loot in LootList.SelectedItems) {
+				loot.SetNominal(percentage);
+			}
 
-            foreach (LootType loot in LootList.SelectedItems)
-            {
-                loot.SetNominal(percentage);
-            }
+			LootList.Items.Refresh();
 
-            LootList.Items.Refresh();
+			PercBox.Text = "0";
+			UpdatePercValue();
+		}
 
-            PercBox.Text = "0";
-            UpdatePercValue();
-        }
+		private void PercSilder_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+			PercBox.Text = Math.Round(PercSilder.Value).ToString();
+			UpdatePercValue();
+		}
 
-        private void PercSilder_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            PercBox.Text = Math.Round(PercSilder.Value).ToString();
-            UpdatePercValue();
-        }
-        
-        private void PercBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            PercBox.SelectAll();
-        }
+		private void PercBox_GotFocus(object sender, RoutedEventArgs e) {
+			PercBox.SelectAll();
+		}
 
-        private void PercBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            UpdatePercValue();
-        }
+		private void PercBox_LostFocus(object sender, RoutedEventArgs e) {
+			UpdatePercValue();
+		}
 
-        private void PercBox_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                UpdatePercValue();
-            }
-        }
-        
-        private void UpdatePercValue()
-        {
-            int newval = 0;
-            bool ok = int.TryParse(PercBox.Text.Replace("%", ""), out newval);
+		private void PercBox_KeyUp(object sender, KeyEventArgs e) {
+			if (e.Key == Key.Enter) {
+				UpdatePercValue();
+			}
+		}
 
-            if (ok)
-            {
-                PercSilder.Value = newval;
-                PercBox.Text = String.Format("{0}%", newval);
-                PercBtn?.Focus();
-            }
-        }
+		private void UpdatePercValue() {
+			int newval = 0;
+			bool ok = int.TryParse(PercBox.Text.Replace("%", ""), out newval);
 
-        private void SaveBtn_Click(object sender, RoutedEventArgs e)
-        {
-            LootTable.SaveFile();
-        }
+			if (ok) {
+				PercSilder.Value = newval;
+				PercBox.Text = String.Format("{0}%", newval);
+				PercBtn?.Focus();
+			}
+		}
 
-        private void PreviewKeyDownHandler(object sender, KeyEventArgs e)
-        {
-            switch (e.Key)
-            {
-                case Key.Delete:
-                case Key.Back:
-                    foreach (LootType loot in LootList.SelectedItems)
-                    {
-                        loot.RemoveType();
-                    }
-                    break;
-            }
-        }
-    }
+		private void SaveBtn_Click(object sender, RoutedEventArgs e) {
+			LootTable.SaveFile();
+		}
+
+		private void PreviewKeyDownHandler(object sender, KeyEventArgs e) {
+			switch (e.Key) {
+				case Key.Delete:
+				case Key.Back:
+					foreach (LootType loot in LootList.SelectedItems) {
+						loot.RemoveType();
+					}
+
+					break;
+			}
+		}
+
+		private void ChangeSearchFilter(Object sender, TextChangedEventArgs e) {
+			if (!(sender is TextBox textBox)) {
+				Debug.WriteLine("What?");
+				return;
+			}
+
+			LootList.ItemsSource = LootTable.Loot.Where(l => StrContains(l.Name, textBox.Text, StringComparison.CurrentCultureIgnoreCase));
+			this.LootList.Items.Refresh();
+			Debug.WriteLine("Updated");
+		}
+
+		public static bool StrContains(string source, string toCheck, StringComparison comp) {
+			return source?.IndexOf(toCheck, comp) >= 0;
+		}
+	}
 }
